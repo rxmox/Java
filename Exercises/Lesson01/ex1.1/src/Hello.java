dfcaff7d87a01fcd55d4a1413f9635285940f795
Omar Ahmed <omar52ramos@gmail.com>
2024-01-11 09:00:15 -0700

    added some of 1st lesson

---
+++ Exercises/Lesson01/ex1.1/src/Hello.java
@@ -0,0 +1,18 @@
+/*
+Author: Omar Ahmed
+Current Version: 1.1
+*/
+
+
+public class Hello {
+    public static void main(String[] args) {
+        //System.out.println("Hello, world");
+
+        //boolean ExampleVariable = true;
+        //System.out.println("Hello? " + ExampleVariable);
+
+        double ExampleVariable = 1.5;
+        System.out.println("Hello, " + ExampleVariable);
+    }
+}
+
