79f74f83f7eaf0fa78069733834ae2dbe40aa3df
Omar Ahmed <omar52ramos@gmail.com>
2024-01-11 11:38:46 -0700

    Completed Exercise 1 and Lab 1

--- Exercises/Lesson01/ex1.1/src/Hello.java
+++ Exercises/Lesson01/ex1.1/src/Hello.java
@@ -1,18 +1,33 @@
-/*
-Author: Omar Ahmed
-Current Version: 1.1
+/**
+@author Omar Ahmed  <a href="mailto:omar.ahmed@ucalgary.ca">omar.ahmed@ucalgary.ca</a>
+@version  1.2
+ @since 1.0
 */
 
+/*
+Hello is a simple program which prints the classic message:
+Hello, world
+ */
 
 public class Hello {
     public static void main(String[] args) {
-        //System.out.println("Hello, world");
+        /*
+          This prints "Hello, world" to the terminal window
+          @param args Optional command-line argument
+         */
+        System.out.println("Hello, world");
 
         //boolean ExampleVariable = true;
         //System.out.println("Hello? " + ExampleVariable);
 
-        double ExampleVariable = 1.5;
-        System.out.println("Hello, " + ExampleVariable);
+        //char ExampleVariable = 'x';
+        //System.out.println("Hello? " + ExampleVariable);
+
+        //int ExampleVariable = 6;
+        //System.out.println("Hello? " + ExampleVariable);
+
+        //double ExampleVariable = 1.5;
+        //System.out.println("Hello, " + ExampleVariable);
     }
-}
+} //End of class declaration
 
