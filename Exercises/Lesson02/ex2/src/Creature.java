865a45308662f0d505c041144a004a1559465500
Omar Ahmed <omar52ramos@gmail.com>
2024-01-12 18:55:58 -0700

    small edits

---
+++ Exercises/Lesson02/ex2/src/Creature.java
@@ -0,0 +1,8 @@
+public class Creature{
+    /**
+     * @param args Handles command-line argument
+     */
+    public static void main(String[] args){
+        System.out.println("This is a placeholder for Creature " + args[0]);
+    }
+}
